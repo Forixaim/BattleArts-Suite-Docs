7a0f8db75d356ab34e422afa75ab45faa5b5bd84
Matthew <thebest20067@gmail.com>
2025-01-23 23:36:16 +0000

    fix 2

---
+++ src/data/home.ts
@@ -0,0 +1,25 @@
+
+const homeInfo = [
+    {
+        "content": "Battle Arts is a suite of mods for Epic Fight that enhances how the mod is played and implements a basic RPG class system to it",
+        "key": 0
+    },
+    {
+        "content": "The suite of mods include the two main library mods, Battle Arts API and ExCap each doing their own purpose.",
+        "key": 1
+    },
+    {
+        "content": "ExCap is primarily used as an enhancement to the base Weapon Capability to make it extendable with the interfaces and layers that the mod provides. It allows the use of assignign custom guard animations, weapon passive skills, and eventually even weapon draw animations.",
+        "key": 2
+    },
+    {
+        "content": "Battle Arts API is used to facilitate the creation of Battle Styles and Active Skills that can be pressed with a key. Active skills are broken into 3 categories, the commonly used Combat Art, a Burst Art to face more uncommon enemies, and a powerful Ultimate Art to really secure the win. The Battle Style also facilitates the usage of Unarmed attacks which can be chained together with Combat Arts and the such.",
+        "key": 3
+    },
+    {
+        "content": "Mod Developers feel free to look around at the source code for the library mods.",
+        "key": 4
+    }
+]
+
+export default homeInfo;
